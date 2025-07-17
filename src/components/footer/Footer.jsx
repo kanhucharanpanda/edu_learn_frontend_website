10545dc914b8612c26e55d4b64437d6ea361f386
kanhucharanpanda <163461359+kanhucharanpanda@users.noreply.github.com>
2025-07-17 21:29:14 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/newsletter/subscribe",
+        "https://edu-learn-server-website.onrender.com/api/newsletter/subscribe",
         {
           method: "POST",
           headers: {
